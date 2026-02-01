756a50af1c5a3e0dc736d84887fc0f00a6d938eb
Your Name <your@email>
2026-02-01 21:20:14 +0300

    UPD \"

--- src/io/libs/HranService.groovy
+++ src/io/libs/HranService.groovy
@@ -12,14 +12,14 @@ class HranService implements Serializable {
     int sync(String rep1c, String repGitLocal, String repGitRemote, String ext = "", String additionalParameters = "", String server1c = "", String storageUser = "", String storagePwd = "") {
         def user = storageUser?.trim() ? storageUser : ctx.env("login_hran")
         def pwd = storagePwd?.trim() ? storagePwd : ctx.env("pass_hran")
-        def command = "gitsync sync --storage-user ${ctx.escapeArg(user)} --storage-pwd ${ctx.escapeArg(pwd)} ${ext} ${additionalParameters} \"${rep1c}\" \"${repGitLocal}\""
+        def command = "gitsync sync --storage-user \"${ctx.escapeArg(user)}\" --storage-pwd \"${ctx.escapeArg(pwd)}\" ${ext} ${additionalParameters} \"${rep1c}\" \"${repGitLocal}\""
         return runner.run(command)
     }
 
     int init(String rep1c, String repGitLocal, String ext = "", String server1c = "", String storageUser = "", String storagePwd = "") {
         //def user = storageUser?.trim() ? storageUser : ctx.env("login_hran")
         //def pwd = storagePwd?.trim() ? storagePwd : ctx.env("pass_hran")
-        def command = "gitsync init --storage-user ${storageUser} --storage-pwd ${storagePwd} ${ext} \"${rep1c}\" \"${repGitLocal}\""
+        def command = "gitsync init --storage-user \"${storageUser}\" --storage-pwd \"${storagePwd}\" ${ext} \"${rep1c}\" \"${repGitLocal}\""
         return runner.run(command)
     }
 }
